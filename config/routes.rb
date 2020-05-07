afefdb1c5b2bb86ace92a7161d707532c4d9ecc9
Expedito Andrade <expeditojazz@gmail.com>
2020-05-07 07:10:30 -0300

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/consumer'
-  get 'static_pages/contact'
   root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/consumer', to: 'static_pages#consumer'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test 'layout links' do
+    get root_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+  end
+end
